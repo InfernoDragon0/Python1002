1481ab96fc2acf46dfd44904ef15e118f2c98c32
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-10-22 18:30:50 +0800

    added handlebars (must npm install)

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var port = 5000;
 var spawn = require('child_process').spawn;
-
+var hbs = require('express-handlebars');
 
 app.listen(port, () => {
     console.log("server started in port " + port)
 })
+app.set('view engine', 'hbs');
+
+app.engine( 'hbs', hbs( {
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+
+app.use(express.static('public'))
 
 app.get('/test', (req,res) => {
-    console.log("something connected")
     var process = spawn('python', ['./main.py'])
-    var finalData = "start: "
+    var finalData = ""
     process.stdout.on('data', function (data) {
-        // var options = {
-        //     chart: {
-        //       type: 'line'
-        //     },
-        //     series: [{
-        //       name: 'sales',
-        //       data: data
-        //     }],
-        //     xaxis: {
-        //       categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-        //     }
-        //   }
-          
-        //   var chart = new ApexCharts(document.querySelector("#chart"), options);
-          
-        //   chart.render();
-        console.log(data.toString())
+        //console.log(data.toString())
         finalData += data.toString()
     });
     process.stderr.on('data', function (data) {
-      console.log(data.toString());
+      //console.log(data.toString());
       finalData += data.toString()
   });
     process.stdout.on('end', function () {
-      res.send(finalData)
+      res.render('index', {layout: 'default', chartData: finalData})
   });
-  process.on('end', function (data) {
-    console.log(finalData)
-    res.send(finalData)
+  process.on('end', function () {
+    ///console.log(finalData)
+    //res.send(finalData)
   })
 })
