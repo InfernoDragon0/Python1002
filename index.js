9728cabe80c8b95c1874c4e1d273f43582e98b07
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-10-22 13:16:46 +0800

    new changes

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var port = 3001;
+var spawn = require('child_process').spawn;
+
+
+app.listen(port, () => {
+    console.log("server ready")
+})
+
+app.get('/test', (req,res) => {
+    var process = spawn('python', ['./pyscripts/main.py'])
+    process.stdout.on('data', function (data) {
+        var options = {
+            chart: {
+              type: 'line'
+            },
+            series: [{
+              name: 'sales',
+              data: data
+            }],
+            xaxis: {
+              categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+            }
+          }
+          
+          var chart = new ApexCharts(document.querySelector("#chart"), options);
+          
+          chart.render();
+        res.send("<script></script>");
+    });
+})
